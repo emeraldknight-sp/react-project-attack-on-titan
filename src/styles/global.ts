030649f01a467fe409ab93d83d77c274a51d8a83
David <contato.davidalmeida@outlook.com>
2024-01-27 17:12:14 -0300

    💄 Add or update the UI and style files

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -20,6 +20,7 @@ time, mark, audio, video, button {
 	font-size: 100%;
 	font: inherit;
 	vertical-align: baseline;
+	text-decoration: none;
 }
 article, aside, details, figcaption, figure,
 footer, header, hgroup, menu, nav, section {
@@ -42,5 +43,40 @@ q:before, q:after {
 table {
 	border-collapse: collapse;
 	border-spacing: 0;
+}
+
+:root {
+	--primary-color: #FF0000;
+	--secondary-color: #FFC400;
+
+	--text-5xl: 3rem;
+	--line-height-5xl: 1;
+
+	--text-4xl: 2.25rem;
+	--line-height-4xl: 2.5rem;
+
+	--text-3xl: 1.875rem;
+	--line-height-3xl: 2.25rem;
+
+	--text-2xl: 1.5rem;
+	--line-height-2xl: 2rem;
+
+	--text-xl: 1.25rem;
+	--line-height-xl: 1.75rem;
+
+	--text-lg: 1.125rem;
+	--line-height-lg: 1.75rem;
+
+	--text-md: 1rem;
+	--line-height-md: 1.5rem;
+
+	--text-sm: 0.875rem;
+	--line-height-sm: 1.25rem;
+
+	--text-xs: 0.75rem;
+	--line-height-xs: 1rem;
+
+	--text-title: "Bebas Neue", sans-serif;
+	--text-content: "Inter", sans-serif;
 }
 `;
