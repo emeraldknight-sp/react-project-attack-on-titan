3afe8d5983057fa3a506bf918d04fe0cd95dd170
David <contato.davidalmeida@outlook.com>
2024-01-27 17:11:58 -0300

    🤡 Mock things

---
+++ src/mock/menuOptions.mock.ts
@@ -0,0 +1,27 @@
+export const menuOptions = [
+  {
+    link: "/",
+    name: "Início",
+    description: "voltar ao início",
+  },
+  {
+    link: "/",
+    name: "Filmes",
+    description: "conferir lista de filmes",
+  },
+  {
+    link: "/",
+    name: "Seriados",
+    description: "conferir lista de seriados",
+  },
+  {
+    link: "/",
+    name: "Desenhos",
+    description: "conferir lista de desenhos",
+  },
+  {
+    link: "/",
+    name: "Favoritos",
+    description: "conferir lista de favoritos",
+  },
+];
