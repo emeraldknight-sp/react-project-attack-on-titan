360b04e012b4dbc95b2fb01a7fa9f066a90251b1
David <contato.davidalmeida@outlook.com>
2024-01-27 17:09:08 -0300

    🏷️ Add or update types

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,1 +1,31 @@
 /// <reference types="react-scripts" />
+
+interface ButtonBaseProps {
+  children: React.ReactNode;
+  type: "button" | "reset" | "submit" | undefined;
+  color: "primary" | "secondary" | string;
+  variant: "text" | "contained" | "outlined";
+  ariaLabel: string;
+  disabled?: boolean;
+  onClick?: () => void;
+}
+
+interface ButtonProps extends ButtonBaseProps {
+  size: "sm" | "md" | "lg";
+}
+
+interface IconButtonProps extends ButtonProps {
+  size: "sm" | "md";
+}
+
+type StyledIconButtonProps = pick<
+  IconButtonProps,
+  "color",
+  "variant",
+  "size"
+>;
+
+interface ContainerProps {
+  children: React.ReactNode;
+  className: string;
+}
