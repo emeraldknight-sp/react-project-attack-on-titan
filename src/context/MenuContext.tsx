d903e3d4e75e7a48f081d61030515eb0c27cdd07
David <contato.davidalmeida@outlook.com>
2024-02-25 11:47:39 -0300

    ✅ Add: MenuContext context

---
+++ src/context/MenuContext.tsx
@@ -0,0 +1,22 @@
+import { createContext, useState } from "react";
+
+
+
+export const MenuContext = createContext<MenuContextProps>({
+  openMenu: false,
+  handleOpenMenu: () => {},
+});
+
+export const MenuContextProvider = ({ children }: MenuContextProviderProps) => {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const handleOpenMenu = () => {
+    setOpenMenu(!openMenu);
+  };
+
+  return (
+    <MenuContext.Provider value={{ openMenu, handleOpenMenu }}>
+      {children}
+    </MenuContext.Provider>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -5,19 +5,22 @@ import { Metric } from "web-vitals";
 import { App } from "./App";
 import { Global } from "@emotion/react";
 import { ResetCSS } from "./styles/global";
+import { MenuContextProvider } from "./context/MenuContext";
 
 const handleWebVitals = (metric: Metric) => {
   console.log(metric);
 };
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
     <Global styles={ResetCSS} />
-    <App />
-  </React.StrictMode>
+    <MenuContextProvider>
+      <App />
+    </MenuContextProvider>
+  </React.StrictMode>,
 );
 
 reportWebVitals(handleWebVitals);
