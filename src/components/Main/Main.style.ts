d98ac98a3bbbba92aab4f811f05a6bd81e5309eb
David <contato.davidalmeida@outlook.com>
2024-02-28 11:48:10 -0300

    💄 Add or update the UI and style file

--- src/components/Main/Main.style.ts
+++ src/components/Main/Main.style.ts
@@ -2,6 +2,9 @@ import styled from "@emotion/styled";
 import { Container } from "../Container";
 
 export const StyledMain = styled.main`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
   padding: 1rem 0;
   flex: 1;
 `;
@@ -10,9 +13,11 @@ export const ContainerMain = styled(Container)`
   display: flex;
   flex-direction: column;
   height: 100%;
-  gap: 100px;
+  gap: 32px;
 
   @media (min-width: 1024px) {
     flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
   }
 `;
