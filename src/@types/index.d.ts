3f15035bbfa970630a139a5f86eb373fc394f345
David <contato.davidalmeida@outlook.com>
2024-02-28 11:48:32 -0300

    🏷️ Add or update types

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="react-scripts" />
 
 interface ButtonProps {
-  onClick: (e: any) => void;
+  onClick?: (e: any) => void;
   disabled?: boolean;
   className?: string;
   style?: React.CSSProperties;
@@ -76,6 +76,75 @@ interface Movie {
   vote_count: number;
 }
 
+interface Season {
+  air_date: string;
+  episode_count: number;
+  id: number;
+  name: string;
+  overview: string;
+  poster_path: string | null;
+  season_number: number;
+  vote_average: number;
+}
+
+interface Episode {
+  id: number;
+  name: string;
+  overview: string;
+  vote_average: number;
+  vote_count: number;
+  air_date: string;
+  episode_number: number;
+  episode_type: string;
+  production_code: string;
+  runtime: number;
+  season_number: number;
+  show_id: number;
+  still_path: string | null;
+}
+
+interface Network {
+  id: number;
+  logo_path: string | null;
+  name: string;
+  origin_country: string;
+}
+
+interface TVSerie {
+  adult: boolean;
+  backdrop_path: string | null;
+  created_by: any[];
+  episode_run_time: number[];
+  first_air_date: string;
+  genres: Genre[];
+  homepage: string;
+  id: number;
+  in_production: boolean;
+  languages: string[];
+  last_air_date: string;
+  last_episode_to_air: Episode;
+  name: string;
+  next_episode_to_air: Episode | null;
+  networks: Network[];
+  number_of_episodes: number;
+  number_of_seasons: number;
+  origin_country: string[];
+  original_language: string;
+  original_name: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  production_companies: ProductionCompany[];
+  production_countries: ProductionCountry[];
+  seasons: Season[];
+  spoken_languages: SpokenLanguage[];
+  status: string;
+  tagline: string;
+  type: string;
+  vote_average: number;
+  vote_count: number;
+}
+
 interface MovieResult {
   adult: boolean;
   backdrop_path: string | null;
@@ -125,4 +194,8 @@ interface MenuContextProviderProps {
 interface MenuContextProps {
   openMenu: boolean;
   handleOpenMenu: () => void;
+}
+
+interface StyledMenuProps {
+  openMenu: boolean;
 }
