9c661010b1b8bdd9c645a0775b65569614c6484f
David <contato.davidalmeida@outlook.com>
2024-03-02 10:40:27 -0300

    🏷️ Add or update types

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -20,6 +20,122 @@ interface ContainerProps {
   className?: string;
 }
 
+interface MainProps {
+  children: React.ReactNode;
+}
+
+interface MenuContextProviderProps {
+  children: React.ReactNode;
+}
+
+interface MenuContextProps {
+  openMenu: boolean;
+  handleOpenMenu: () => void;
+}
+
+interface StyledMenuProps {
+  openMenu: boolean;
+}
+
+interface Network {
+  id: number;
+  logo_path: string | null;
+  name: string;
+  origin_country: string;
+}
+
+interface LastEpisodeToAir {
+  id: number;
+  name: string;
+  overview: string;
+  vote_average: number;
+  vote_count: number;
+  air_date: string;
+  episode_number: number;
+  episode_type: string;
+  production_code: string;
+  runtime: number;
+  season_number: number;
+  show_id: number;
+  still_path: string | null;
+}
+
+interface Season {
+  air_date: string;
+  episode_count: number;
+  id: number;
+  name: string;
+  overview: string;
+  poster_path: string | null;
+  season_number: number;
+  vote_average: number;
+}
+
+interface ProductionCompany {
+  id: number;
+  logo_path: string | null;
+  name: string;
+  origin_country: string;
+}
+
+interface Genre {
+  id: number;
+  name: string;
+}
+
+interface ProductionCountry {
+  iso_3166_1: string;
+  name: string;
+}
+
+interface SpokenLanguage {
+  english_name: string;
+  iso_639_1: string;
+  name: string;
+}
+
+interface TVSeries {
+  adult: boolean;
+  backdrop_path: string | null;
+  created_by: any[];
+  episode_run_time: number[];
+  first_air_date: string;
+  genres: Genre[];
+  homepage: string;
+  id: number;
+  in_production: boolean;
+  languages: string[];
+  last_air_date: string;
+  last_episode_to_air: LastEpisodeToAir;
+  name: string;
+  next_episode_to_air: null | LastEpisodeToAir;
+  networks: Network[];
+  number_of_episodes: number;
+  number_of_seasons: number;
+  origin_country: string[];
+  original_language: string;
+  original_name: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  production_companies: ProductionCompany[];
+  production_countries: ProductionCountry[];
+  seasons: Season[];
+  spoken_languages: SpokenLanguage[];
+  status: string;
+  tagline: string;
+  type: string;
+  vote_average: number;
+  vote_count: number;
+}
+
+interface BelongsToCollection {
+  id: number;
+  name: string;
+  poster_path: string | null;
+  backdrop_path: string | null;
+}
+
 interface Genre {
   id: number;
   name: string;
@@ -41,13 +157,6 @@ interface SpokenLanguage {
   english_name: string;
   iso_639_1: string;
   name: string;
-}
-
-interface BelongsToCollection {
-  id: number;
-  name: string;
-  poster_path: string | null;
-  backdrop_path: string | null;
 }
 
 interface Movie {
@@ -78,97 +187,22 @@ interface Movie {
   vote_count: number;
 }
 
-interface Season {
-  air_date: string;
-  episode_count: number;
-  id: number;
+interface Trailer {
+  iso_639_1: string;
+  iso_3166_1: string;
   name: string;
-  overview: string;
-  poster_path: string | null;
-  season_number: number;
-  vote_average: number;
-}
-
-interface Episode {
-  id: number;
-  name: string;
-  overview: string;
-  vote_average: number;
-  vote_count: number;
-  air_date: string;
-  episode_number: number;
-  episode_type: string;
-  production_code: string;
-  runtime: number;
-  season_number: number;
-  show_id: number;
-  still_path: string | null;
-}
-
-interface Network {
-  id: number;
-  logo_path: string | null;
-  name: string;
-  origin_country: string;
-}
-
-interface TVSerie {
-  adult: boolean;
-  backdrop_path: string | null;
-  created_by: any[];
-  episode_run_time: number[];
-  first_air_date: string;
-  genres: Genre[];
-  homepage: string;
-  id: number;
-  in_production: boolean;
-  languages: string[];
-  last_air_date: string;
-  last_episode_to_air: Episode;
-  name: string;
-  next_episode_to_air: Episode | null;
-  networks: Network[];
-  number_of_episodes: number;
-  number_of_seasons: number;
-  origin_country: string[];
-  original_language: string;
-  original_name: string;
-  overview: string;
-  popularity: number;
-  poster_path: string | null;
-  production_companies: ProductionCompany[];
-  production_countries: ProductionCountry[];
-  seasons: Season[];
-  spoken_languages: SpokenLanguage[];
-  status: string;
-  tagline: string;
+  key: string;
+  site: string;
+  size: number;
   type: string;
-  vote_average: number;
-  vote_count: number;
+  official?: boolean;
+  published_at?: string;
+  id: string;
 }
 
-interface Movie {
-  adult: boolean;
-  backdrop_path: string | null;
-  genre_ids: number[];
+interface MovieTrailers {
   id: number;
-  original_language: string;
-  original_title: string;
-  overview: string;
-  popularity: number;
-  poster_path: string | null;
-  release_date: string;
-  title: string;
-  video: boolean;
-  vote_average: number;
-  vote_count: number;
-}
-
-interface MovieList {
-  page: number;
-  results: MovieResult[];
-  total_pages: number;
-  total_results: number;
+  results: Trailer[];
 }
 
 interface Trailer {
@@ -184,20 +218,7 @@ interface Trailer {
   id: string;
 }
 
-interface Trailers {
+interface TVSeriesTrailers {
   id: number;
   results: Trailer[];
-}
-
-interface MenuContextProviderProps {
-  children: React.ReactNode;
-}
-
-interface MenuContextProps {
-  openMenu: boolean;
-  handleOpenMenu: () => void;
-}
-
-interface StyledMenuProps {
-  openMenu: boolean;
 }
